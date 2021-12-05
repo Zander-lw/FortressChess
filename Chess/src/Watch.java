766e9020fcce6509b756f2157dd2937865c72682
C-flo7 <93959560+C-flo7@users.noreply.github.com>
2021-12-04 22:24:34 -0600

    Update Watch.java

--- Chess/src/Watch.java
+++ Chess/src/Watch.java
@@ -3,13 +3,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
-public class Watch {
+public class Watch extends JPanel{
 	
-	JFrame SettingFrame;
 	JLabel Label;
 	Font fontsize = new Font("Arial", Font.PLAIN, 20);	
 	Timer timer;	
@@ -17,22 +16,20 @@ public class Watch {
 	String SecString, MinString;	
 	DecimalFormat dFormat = new DecimalFormat("00");
 	
-	public Watch() {
+	public Watch(int time) {
 		
-		SettingFrame = new JFrame();
-		SettingFrame.setSize(300,400);
+		setSize(300,400);
 		
 		Label = new JLabel("");
 		Label.setBounds(100, 100, 100, 30);
 		Label.setHorizontalAlignment(JLabel.CENTER);
 		Label.setFont(fontsize);
 		
-		SettingFrame.add(Label);
-		SettingFrame.setVisible(true);
+		add(Label);
+		setVisible(true);
 		
-		Label.setText("01:00");
-		second = 0;
-		minute = 1;
+		second = time % 60;
+		minute = (time/60);
 		Timer();
 		timer.start();						
 	}
