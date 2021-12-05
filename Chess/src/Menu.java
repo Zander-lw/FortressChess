54d49ca3e11e3857a05261eea49ca3226af13be7
C-flo7 <93959560+C-flo7@users.noreply.github.com>
2021-12-04 18:33:03 -0600

    Update Menu.java

--- Chess/src/Menu.java
+++ Chess/src/Menu.java
@@ -119,8 +119,28 @@ public class Menu extends Frame implements ActionListener
   private void instructionsActionPerformed(ActionEvent evt) 
   {         
       JOptionPane.showMessageDialog(menu,
-      "Put all the rules here",
-      "How to Play", JOptionPane.PLAIN_MESSAGE);
+      "Piece Movement:\n"
+        + "Kings move one square in any direction, so long as that square is not attacked by an enemy piece. Additionally, kings are able to make a special move, known as castling.\n"
+        + "Queens move diagonally, horizontally, or vertically any number of squares. They are unable to jump over pieces. (9 Points)\n"
+        + "Rooks move horizontally or vertically any number of squares. They are unable to jump over pieces. Rooks move when the king castles. (5 Points)\n"
+        + "Bishops move diagonally any number of squares. They are unable to jump over pieces. (3 Points)\n"
+        + "Knights move in an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically. They are the only piece able to jump over other pieces. (3 Points)\n"
+        + "Pawns move vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move.\n"
+        + "The pawns capture one square diagonally in a forward direction. Pawns are unable to move backward on captures or moves. \nUpon reaching the other side of the board a pawn promotes into any other piece, except for a king. (1 Point)\n\n"  
+      + "Gameplay:\n"
+        + "Each player takes turns moving one of their own pieces. To move a piece, click on one of your pieces and then click on one of its valid movement squares.\n
+        + "If one of these squares is an enemy piece that piece is 'captured' and removed from the board.\n"
+        + "There is an individual turn timer: if you dont move a piece in the alloted time your turn is skipped.\nThere is also a game timer, if that runs out the winner is determined by who has the most points.\n"
+        + "Additionally, there are multiple boards to choose/play on.\n\n"
+      + "Win Conditions:\n"
+        + "There are two win main conditions that can be chosen in the settings menu.\n"
+        + "The first win condition is known as a checkmate. When one of your pices is 'attacking' the opposing king (or vice versa), this is known as a check.\n" 
+        + "When In check, the king must get out of check by either moving to a safe square, having one of its pieces block the attacker, or having a piece capture the attacker.\n"
+        + "If none of these options are possible then it is considered checkmate the side giving checkmate wins.\n"
+        + "Be careful: if the opposing king has no legal moves and is not currently in check then the result is a draw by stalemate.\n"
+        + "The other main win condition is getting your king to the opposite side of the board, achieving this before your opponent is a win.\n"
+        + "A default win condition that cannot be changed occurs when the game timer runs out. Each sides' captured pieces are converted to their specified points and added up. Whoever captured the most ponts is the winner, if the poiints are equal the result is a draw.
+      ,"How to Play", JOptionPane.PLAIN_MESSAGE);
   }                                            
    /**
    * Listens for when the settings button is clicked
