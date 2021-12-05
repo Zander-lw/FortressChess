c563c7a8f4ca8fd253376621ea80b0fef1f5bfbe
BenEatBread <93959605+BenEatBread@users.noreply.github.com>
2021-12-04 20:55:59 -0600

    Add files via upload

--- Chess/src/Game.java
+++ Chess/src/Game.java
@@ -1,6 +1,75 @@
-public class Game {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Game 
+{
+
+	JFrame Frame = new JFrame("Chess");
+	SettingsDisplay Win = new SettingsDisplay(null);
+	JButton SurrenderButton = new JButton("Surrender");
+	int TimePass = 0;
+	int TurnTracker = 0;
+	int PieceCapture = 0;
 	
-	public static void main(String[] args) {
+	
+	public int TurnTracker()
+	{
+		TurnTracker++;
+		return TurnTracker;
+	}
+	Game()
+	{
+		Frame.add(SurrenderButton);  
+		
+		SurrenderButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent Surrender) {
+	        Frame.dispose();
+	        winMessage();
+	    }
+	  });
+	
+	}
+	 public void winMessage()
+	 {
+			Frame.setSize(300,400);    
+			Frame.setLayout(null);    
+			Frame.setVisible(true);    
+			Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+	        JOptionPane.showMessageDialog(Frame,
+	                "Congratulations \n"
+	                + "You have win the game\n",
+	                "Winner", JOptionPane.PLAIN_MESSAGE);
+	}
+	 public void tieMessage()
+	 {
+			Frame.setSize(300,400);    
+			Frame.setLayout(null);    
+			Frame.setVisible(true);    
+			Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+	        JOptionPane.showMessageDialog(Frame,
+	                "The game has end in a draw \n"
+	                + "neither players has won \n",
+	                "Draw", JOptionPane.PLAIN_MESSAGE);
+	}
+	public void win()
+	{
+		Frame.dispose();
+		winMessage();
+	}
+	public void tie() 
+	{
+		Frame.dispose();
+		tieMessage();
+	}
+	public static void main(String[] args) 
+	{
 		
 		Menu StartMenu = new Menu();
 		//Watch Timer = new Watch();
---
+++ Chess/src/Player.java
@@ -0,0 +1,26 @@
+
+public class Player {
+
+	
+	public int Score = 0;
+	public int Score2 = 0;
+	
+	public int getNum() 
+	{
+		return Score;
+	}
+	public void setNum(int Points) {
+	    this.Score = Points;
+	}
+	public int getNum2() 
+	{
+		return Score2;
+	}
+	public void setNum2(int Point2) {
+	    this.Score2 = Point2;
+	}
+	
+	
+	
+	
+}
