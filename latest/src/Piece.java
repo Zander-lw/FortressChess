d7e26a78ce5d3e2299f68d64953e23e2c518a58c
J-049 <95116498+J-049@users.noreply.github.com>
2021-12-05 23:36:08 -0600

    Add files via upload

--- latest/src/Piece.java
+++ latest/src/Piece.java
@@ -6,8 +6,9 @@
 import java.util.*;
 
 /** This parent class defines all shared attributes
-	and methods inherited by specialized chess 
-	pieces*/
+*	and methods inherited by specialized chess 
+*	pieces
+*/
 public class Piece
 {
 	private String type;
@@ -18,6 +19,14 @@ public class Piece
 	private int points;
 	private boolean isCaptured;
 
+	/** Piece Constructer takes in assigned attributes
+	*	and sets all others to default values
+	*	
+	*	@param type		the type of piece
+	*	@param number 	the number associated with it, ie: pawn1, pawn2
+	*	@param color 	the color of the piece
+	*	@param initPos	the initial position of the piece
+	*/
 	public Piece(String type, int number, String color, String initPos)
 	{
 		this.type = type;
@@ -31,47 +40,84 @@ public class Piece
 			points = 2;
 		isCaptured = false;
 	}
-		
+	
+	/** Method to return the piece type
+	*	
+	*	@return type 	the type of piece
+	*/
 	public String getType()
 	{
 		return type;
 	}
 	
+	/** Method to return the piece number
+	*	
+	*	@return number 	the number associated with it, ie: pawn1, pawn2
+	*/
 	public int getNumber()
 	{
 		return number;
 	}
 	
+	/** Method to return the piece color
+	*	
+	*	@return color 	the color of the piece
+	*/
 	public String getColor()
 	{
 		return color;
 	}
 	
+	/** Method to return the current position of the piece 
+	*	
+	*	@return position 	the current position of the piece
+	*/
 	public String getPosition()
 	{
 		return position;
 	}
 	
+	/** Method to return the initial position of the piece 
+	*	
+	*	@return initPos 	the initial position of the piece
+	*/
 	public String getInitPos()
 	{
 		return initPos;
 	}
 	
+	/** Method to return the point value associated with the piece 
+	*	
+	*	@return points 	the point value of the piece
+	*/
 	public int getPoints()
 	{
 		return points;
 	}
 	
+	/** Method to return the boolean value that determines
+	*	whether or not the piece is captured
+	*	
+	*	@return isCaptured 	the capture status of the piece
+	*/
 	public boolean getCaptured()
 	{
 		return isCaptured;
 	}
 	
+	/** Method to set the piece's isCaptured status to true,
+	* 	thereby rendering it captured 
+	*/
 	public void capture()
 	{
 		isCaptured = true;
 	}
 	
+	/** Method to set the piece's position to a new position,
+	*	keeping track of its movement
+	*
+	*	@param destination 		the destination square name, ie A1, B5, etc...
+	*/
 	public void move(String destination)
 	{
 		position = destination;
